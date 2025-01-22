c02d5023a3c40d88b4b503878ff77940d0a22b67
feliciatrinh <felicia.trinh@gmail.com>
2025-01-21 20:09:19 -0800

    Complete Exercise-1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
+import io
+import pathlib
 import requests
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,10 +13,33 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+DOWNLOADS = '/downloads'
+
+def create_directory_if_not_exists(path):
+    pathlib.Path(path).mkdir(exist_ok=True)
+
+
+def download_file(uri):
+    file_name = uri.split('/')[-1].split('.')[0]
+    # download zip file
+    response = requests.get(uri)
+    zipped = ZipFile(io.BytesIO(response.content))
+    # extract zip file and save as a csv
+    zipped.extractall(f'{DOWNLOADS}/{file_name}.csv')
+
 
 def main():
-    # your code here
-    pass
+    create_directory_if_not_exists(DOWNLOADS)
+    for uri in download_uris:
+        try:
+            download_file(uri)
+        except Exception:
+            print(f"Unable to download file from {uri}")
+
+    # print the downloaded file paths
+    downloaded_files = pathlib.Path(DOWNLOADS).iterdir()
+    for file in downloaded_files:
+        print(file)
 
 
 if __name__ == "__main__":
